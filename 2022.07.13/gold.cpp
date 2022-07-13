90ac9b73d59fcf553c838fd43f25600cc71a104b
Dhiraj <dpthorat85@gmail.com>
2022-07-13 23:48:15 +0530

    Day 20

---
+++ 2022.07.13/gold.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vi;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef pair<string, string> pss;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<long long> vl;
+typedef vector<vl> vvl;
+
+#define fastIO                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define maxOfVec(a) *max_element(a.begin(), a.end())
+#define minOfVec(a) *min_element(a.begin(), a.end())
+#define mp make_pair
+#define pb push_back
+#define fr(i, n) for (int i = 0; i < n; i++)
+#define lcm(a, b) (a * b) / (__gcd(a, b))
+#define countBits(n) (log2(n) + 1)
+int mod = 1e9 + 7;
+// ASCII of 'a'-97,'z'-123,'A'-65,'Z'-90,'0'-48,'9'-57
+
+void DhirajThorat()
+{
+    // You are the best !!!!
+}
+
+void Dhiraj()
+{
+    int n, x, sum = 0;
+    cin >> n >> x;
+    int a[n];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    for (int i = 0; i < n; i++)
+    {
+        sum += a[i];
+        if (sum == x && i < n - 1)
+        {
+            sum -= a[i];
+            swap(a[i], a[i + 1]);
+            sum += a[i];
+        }
+    }
+    if (sum == x)
+        cout << "No" << endl;
+    else
+    {
+        cout << "Yes" << endl;
+        for (int i = 0; i < n; i++)
+            cout << a[i] << " ";
+    }
+}
+
+int main()
+{
+
+    fastIO;
+
+    DhirajThorat();
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        Dhiraj();
+        cout << '\n';
+    }
+
+    return 0;
+}
