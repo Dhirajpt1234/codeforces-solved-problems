9f8f3f69dcdc78dd368df3745d85ba672cc5bf6d
Dhiraj <dpthorat85@gmail.com>
2022-07-28 07:51:38 +0530

    day 35

---
+++ 2022.07.28/1moamen.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define int long long
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef pair<string, string> pss;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<long long> vl;
+typedef vector<vl> vvl;
+
+#define fastIO                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define maxOfVec(a) *max_element(a.begin(), a.end())
+#define minOfVec(a) *min_element(a.begin(), a.end())
+#define mp make_pair
+#define pb push_back
+#define fr(i, n) for (int i = 0; i < n; i++)
+#define lcm(a, b) (a * b) / (__gcd(a, b))
+#define countBits(n) (log2(n) + 1)
+int mod = 1e9 + 7;
+// ASCII of 'a'-97,'z'-123,'A'-65,'Z'-90,'0'-48,'9'-57
+
+void DhirajThorat()
+{
+    // You are the best !!!!
+}
+
+void Dhiraj()
+{
+    int n, k;
+    cin >> n >> k;
+
+    vi arr(n);
+    fr(i, n) { cin >> arr[i]; }
+    int cnt = 0;
+    fr(i, n - 1)
+    {
+        if (arr[i] > arr[i + 1])
+            cnt++;
+    }
+
+    if (cnt < k)
+        cout << "YES";
+    else
+        cout << "NO";
+}
+
+int32_t main()
+{
+
+    fastIO;
+    DhirajThorat();
+    int O_O = 1;
+    cin >> O_O;
+    while (O_O--)
+    {
+        Dhiraj();
+        cout << '\n';
+    }
+}
