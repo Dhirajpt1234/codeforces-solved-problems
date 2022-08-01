c66a9cd0f64dddb6fbb5e8cdc261300539014207
Dhiraj <dpthorat85@gmail.com>
2022-08-01 07:13:11 +0530

    day 39

---
+++ 2022.07.31/1repeat.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define int long long
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef pair<string, string> pss;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<long long> vl;
+typedef vector<vl> vvl;
+
+#define fastIO                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define maxOfVec(a) *max_element(a.begin(), a.end())
+#define minOfVec(a) *min_element(a.begin(), a.end())
+#define mkp make_pair
+#define push push_back
+#define pop pop_back
+#define fr(i, n) for (int i = 0; i < n; i++)
+#define lcm(a, b) (a * b) / (__gcd(a, b))
+#define countBits(n) (log2(n) + 1)
+int mod = 1e9 + 7;
+// ASCII of 'a'- 97,'z'- 123,'A'- 65,'Z'- 90,'0'- 48,'9'- 57
+
+void DhirajThorat()
+{
+    // You are the best !!!!
+}
+
+void Dhiraj()
+{
+    int n;
+    cin >> n;
+
+    int arr[n];
+    fr(i, n) { cin >> arr[i]; }
+    int ref = arr[n - 1];
+    int curr = 1;
+    int pre = 1, cnt = 0;
+    int i = n-2;
+    while( i >= 0)
+    {
+        if (arr[i] != ref)
+        {
+            i -= pre;
+            pre *= 2;
+            cnt++;
+        }
+        else
+        {
+            pre++;
+            i--;
+        }
+    }
+
+    cout<<cnt;
+}
+
+int32_t main()
+{
+
+    fastIO;
+    DhirajThorat();
+    int O_O = 1;
+    cin >> O_O;
+    while (O_O--)
+    {
+        Dhiraj();
+        cout << '\n';
+    }
+}
---
+++ 2022.07.31/4fight.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define int long long
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef pair<string, string> pss;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<long long> vl;
+typedef vector<vl> vvl;
+
+#define fastIO                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define maxOfVec(a) *max_element(a.begin(), a.end())
+#define minOfVec(a) *min_element(a.begin(), a.end())
+#define mkp make_pair
+#define push push_back
+#define pop pop_back
+#define fr(i, n) for (int i = 0; i < n; i++)
+#define lcm(a, b) (a * b) / (__gcd(a, b))
+#define countBits(n) (log2(n) + 1)
+int mod = 1e9 + 7;
+// ASCII of 'a'- 97,'z'- 123,'A'- 65,'Z'- 90,'0'- 48,'9'- 57
+
+void DhirajThorat()
+{
+    // You are the best !!!!
+}
+
+void Dhiraj()
+{
+    int hc, dc, hm, dm, k, du, hu, a;
+    cin >> hc >> dc >> hm >> dm >> k >> du >> hu;
+    a = 1;
+    for (int i = 0; i <= k; i++)
+    {
+        if ((hc + i * hu + dm - 1) / dm >= (hm + dc + (k - i) * du - 1) / (dc + (k - i) * du))
+        {
+            cout << "YES\n";
+            a = 0;
+            break;
+        }
+    }
+    if (a)
+        cout << "NO\n";
+}
+
+int32_t main()
+{
+
+    // fastIO;
+    DhirajThorat();
+    int O_O = 1;
+    cin >> O_O;
+    while (O_O--)
+    {
+        Dhiraj();
+        cout << '\n';
+    }
+}
